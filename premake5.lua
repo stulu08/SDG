3e1e20291f4fa43f7fe8e5cec70d600eb1ea8127
derst <derst@GALAXY-BOOK>
2025-09-24 23:10:17 +0200

    Static Runtime off

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,7 @@ project "SDG"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++17"
-	staticruntime "on"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "")
 	objdir ("bin-int/" .. outputdir .. "")
